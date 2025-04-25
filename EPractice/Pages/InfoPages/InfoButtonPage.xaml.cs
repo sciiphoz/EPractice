14957364cd6334aaa13085fdc2e4dd56ae5b6dcf
Блинов Артём Евгеньевич <212202@mck-ktits.ru>
2025-04-25 18:20:47 +0300

    ahfshdfhfds

---
+++ EPractice/Pages/InfoPages/InfoButtonPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EPractice.Pages.InfoPages
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoButtonPage.xaml
+    /// </summary>
+    public partial class InfoButtonPage : Page
+    {
+        public InfoButtonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
